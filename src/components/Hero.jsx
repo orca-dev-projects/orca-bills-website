d2ff3bfa5f3170c36300d573d866dbc3cfb24446
mdazad1508 <mdazad15081999@gmail.com>
2023-08-20 17:33:15 +0530

    line changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
           </div>
         </div>
         <p className="text-white">
-          Yearly subscription starts only @ ₹999 * with lifetime Warranty
+          Yearly subscription starts only @ ₹999 * with lifetime Software support.
         </p>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
